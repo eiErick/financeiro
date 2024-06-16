0fb7ca479e73661d813f3e3caf4e5e280985eeb1
Érick <eierickdasilva@gmail.com>
2024-06-16 18:15:38 -0300

    Init: initial commit

---
+++ theme.js
@@ -0,0 +1,3 @@
+const html = document.documentElement;
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) html.classList.toggle('dark');
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) html.classList.toggle('light');
