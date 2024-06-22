59b524f1955bc6304ca1656a952f8f5216124001
Érick <eierickdasilva@gmail.com>
2024-06-22 14:13:52 -0300

    Feature: storing data over months

--- script.js
+++ script.js
@@ -20,6 +20,33 @@ const valueInput = document.querySelector('.in-ex-screen_value-input');
 const inExScreenCancelBtn = document.querySelector('.in-ex-screen_cancel-btn');
 const inExScreenAddBtn = document.querySelector('.in-ex-screen_add-btn');
 
+const date = new Date();
+const currentDate = `${getMonth()}-${getYear()}`;
+
+let finances = {};
+const savedFinances = localStorage.getItem('savedFinances');
+
+if (savedFinances) {
+    finances = JSON.parse(savedFinances);
+    Object.keys(finances.profile[1]).forEach((e) => createOptionMonth(extractNumbers(e).start, extractNumbers(e).end));
+}
+
+if (selectedMonth.value != currentDate) {
+    let isCurrentMonth = false;
+    selectedMonth.childNodes.forEach((e) => e.value === currentDate ? isCurrentMonth = true : '');
+    if (!isCurrentMonth) createOptionMonth(getMonth(), getYear());
+} else selectedMonth.value = currentDate;
+
+if (savedFinances) {
+    finances.profile[1][currentDate].forEach((e) => printDescription(e.title, e.value, e.type));
+} else {
+    const name = window.prompt('Say the name for your profile!');
+    finances.profile = [name, {}];
+
+    selectedMonth.childNodes.forEach((element) => finances.profile[1][element.value] = []);
+    localStorage.setItem('savedFinances', JSON.stringify(finances));
+}
+
 addIncomeBtn.addEventListener('click', () => {
     showScreen(inExScreen, 'flex');
     inExScreenTitle.textContent = 'Income';
@@ -30,13 +57,27 @@ addExpenseBtn.addEventListener('click', () => {
     inExScreenTitle.textContent = 'Expense';
 });
 
+selectedMonth.addEventListener('change', () => {
+    clearDescription();
+
+    if (finances.profile[1][selectedMonth.value]) {
+        finances.profile[1][selectedMonth.value].forEach((e) => printDescription(e.title, e.value, e.type));
+    } else {
+        finances.profile[1][selectedMonth.value] = [];
+        localStorage.setItem('savedFinances', JSON.stringify(finances));
+    }
+});
+
 returnHomeBtn.addEventListener('click', () => showScreen(home, 'block'));
 inExScreenCancelBtn.addEventListener('click', () => showScreen(home, 'block'));
 
 inExScreenAddBtn.addEventListener('click', () => {
     if (descriptionInput.value === '') return;
     if (valueInput.value === '') return;
-    printDescription(descriptionInput.value, valueInput.value, inExScreenTitle.textContent.toLowerCase());
+
+    if (selectedMonth.value === inExScreenSelectedMonth.value) printDescription(descriptionInput.value, valueInput.value, inExScreenTitle.textContent.toLowerCase());
+
+    saveFinaceDescription(descriptionInput.value, valueInput.value, inExScreenTitle.textContent.toLowerCase(), inExScreenSelectedMonth.value);
 
     descriptionInput.value = '';
     valueInput.value = '';
@@ -51,12 +92,20 @@ function showScreen(element, show) {
 }
 
 function printDescription(title, value, type) {
-    const templete = `
-        <div class="description"><span class="description_icon"><img src="imgs/coin.svg" alt="coin"></span><p class="description_title">${title}</p><span class="description_value"><span class="description_value-type">${type === 'income' ? '+' : '-'} <span>${value}</span><span>$</span></div>
-    `;
+    const template = `<div class="description"><span class="description_icon"><img src="imgs/coin.svg" alt="coin"></span><p class="description_title">${title}</p><span class="description_value"><span class="description_value-type">${type === 'income' ? '+' : '-'} <span>${value}</span><span>$</span></div>`;
 
-    descriptionContainer.innerHTML += templete;
+    descriptionContainer.innerHTML += template;
     updateMonitor();
+}
+
+function clearDescription() {
+    descriptionContainer.innerHTML = '';
+}
+
+function saveFinaceDescription(title, value, type, month) {
+    finances.profile[1][month].push({id: `${randomID()}`,title: title, value: value, type: type});
+
+    localStorage.setItem('savedFinances', JSON.stringify(finances));
 }
 
 function updateMonitor() {
@@ -85,4 +134,39 @@ function getExpense() {
 
 function getBalance() {
     return parseInt(monitorIncome.childNodes[0].textContent) - parseInt(monitorExpense.childNodes[0].textContent);
+}
+
+function getYear() {
+    return date.getFullYear();
+}
+
+function getMonth() {
+    return date.getMonth();
+}
+
+function getNameMonth(num) {
+    const months = ['janary', 'februaty', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+    return months[num];
+}
+
+function createOptionMonth(month, year) {
+    const optionTemplate = `<option value="${month}-${year}">${capitalizeFirstLetter(getNameMonth(month))} ${year}</option>`;
+    
+    selectedMonth.innerHTML += optionTemplate;
+    inExScreenSelectedMonth.innerHTML += optionTemplate;
+    inExScreenSelectedMonth.value = `${month}-${year}`;
+    selectedMonth.value = `${month}-${year}`;
+}
+
+function capitalizeFirstLetter(text) {
+    return text.charAt(0).toUpperCase() + text.substring(1);
+}
+
+function randomID() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+function extractNumbers(inputString) {
+    let parts = inputString.split('-');
+    return { start: parseInt(parts[0], 10), end: parseInt(parts[1], 10) };
 }
